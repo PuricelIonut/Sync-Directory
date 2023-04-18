e18b80bc695e7202e82dd80276b1234524cb0b00
ionut <ionutclaudiu50@yahoo.com>
2023-04-18 16:48:14 +0300

    small progress

--- main.py
+++ main.py
@@ -7,28 +7,13 @@ from sys import argv
 parser = argparse.ArgumentParser(description="Sync two directories")
 
 
-origin = 'C:/Users/ionut/OneDrive/Desktop/programare/learning/sync directory/source/'
-target = 'C:/Users/ionut/OneDrive/Desktop/programare/learning/sync directory/replica/'
-
-files_to_sync = os.listdir(origin)
-
-shutil.copytree(src=origin, dst=target, dirs_exist_ok=True)
-
-#for file_name in files_to_sync:
-   #     shutil.copy2(src=origin+file_name, dst=target+file_name)
-  #      print(f"{file_name} succesfully synced!" )
+origin = 'C:/Users/ionut/Desktop/programare/sync directory/source/'
+target = 'C:/Users/ionut/Desktop/programare/sync directory/replica/'
 
 
-"""def cl_handler(parser):
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        "-eAll", help="Encrypt all files inside root directory", action="store_true"
-    )
-    args = parser.parse_args()
+for file_name in os.listdir(origin):
+    if os.path.isfile((file_name)):
+        shutil.copy2(src=origin+file_name, dst=target+file_name)
+        print(f"{file_name} succesfully synced!" )
 
-    # Check for existing argv
-    if len(argv) < 2:
-        parser.print_help()
-    return vars(args)
 
-"""
