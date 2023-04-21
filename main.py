8d1fcf899b6f6f95f3c6e76a2784bc80466e4e47
Ionut <ionutclaudiu50@yahoo.com>
2023-04-21 22:53:35 +0300

    add list comprehension for return functions

--- main.py
+++ main.py
@@ -7,68 +7,67 @@ from sys import argv
 parser = argparse.ArgumentParser(description="Sync two directories")
 
 
-origin = 'C:/Users/ionut/OneDrive/Desktop/programare/learning/sync directory/source'
-target = 'C:/Users/ionut/OneDrive/Desktop/programare/learning/sync directory/replica'
+origin = "C:/Users/ionut/OneDrive/Desktop/programare/learning/sync directory/source"
+target = "C:/Users/ionut/OneDrive/Desktop/programare/learning/sync directory/replica"
 
 
 # Sync target to match src
-def start_sync(path_origin, path_target):
-    print('Sync in progress...')
-    directories, files = replace_data(path_origin, path_target)
+def start_sync(source, replica):
+    print("Sync in progress...")
+    directories, files = replace_data(source, replica)
     for i in directories:
         if not os.path.exists(i):
             os.mkdir(i)
-            print('Creating directory: '+os.path.normpath(i))
-    for j, k in zip(files, get_files(path_origin)):
+            print("Creating directory: " + os.path.normpath(i))
+    for j, k in zip(files, get_files(source)):
         if not os.path.exists(j) or os.path.getmtime(j) != os.path.getmtime(k):
             shutil.copy2(dst=j, src=k)
-            print('Copying file: '+os.path.normpath(j))
-    remove_files(path_origin=path_origin, path_target=path_target)
-    print('Sync completed!')
+            print("Copying file: " + os.path.normpath(j))
+    remove_files(source, replica)
+    print("Sync completed!")
 
 
-# Remove files and dirs to match src
-def remove_files(path_origin, path_target):
-    directories, files = replace_data(path_origin, path_target)
-    for i in get_files(path_target):
+# Remove files and dirs to match source
+def remove_files(source, replica):
+    directories, files = replace_data(source, replica)
+    for i in get_files(replica):
         if i not in files:
             os.remove(i)
-            print('Deleting file: '+os.path.normpath(i))
-    for j in get_directories(path_target):
+            print("Deleting file: " + os.path.normpath(i))
+    for j in get_directories(replica):
         if j not in directories:
             os.rmdir(j)
-            print('Deleting directory: '+os.path.normpath(j))
+            print("Deleting directory: " + os.path.normpath(j))
 
 
 # Get all files in every subdir inside given path
 def get_files(path):
-    files = []
-    for p in os.walk(path):
-        for file in os.listdir(p[0]):
-            if not os.path.isdir(p[0]+'/'+file):
-                files.append(p[0]+'/'+file)
-    return files
+    return [
+        p[0] + "/" + file
+        for p in os.walk(path)
+        for file in os.listdir(p[0])
+        if not os.path.isdir(p[0] + "/" + file)
+    ]
 
 
 # Get all subdirs based on path
 def get_directories(path):
-    directories = []
-    for f in os.walk(path):
-        directories.append(f[0])
-    return directories
+    return [i[0] for i in os.walk(path)]
 
 
 # Replace src dir name with target dir name - takes origin and target paths
-def replace_data(path_origin, path_target):
-    if not os.path.exists(path_target):
-        os.mkdir(path_target)
+def replace_data(source, replica):
+    if not os.path.exists(replica):
+        os.mkdir(replica)
 
-    # Replace dir name 
-    directories = [x.replace(os.path.basename(path_origin), os.path.basename(path_target)) for x in get_directories(path_origin) ]
-    files = [x.replace(os.path.basename(path_origin), os.path.basename(path_target)) for x in get_files(path_origin) ]
-    return directories, files
-    
+    # Replace dir name
+    return [
+        x.replace(os.path.basename(source), os.path.basename(replica))
+        for x in get_directories(source)
+    ], [
+        x.replace(os.path.basename(source), os.path.basename(replica))
+        for x in get_files(source)
+    ]
 
-start_sync(path_origin=origin, path_target=target)
 
-
+start_sync(source=origin, replica=target)
