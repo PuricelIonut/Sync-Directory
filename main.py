a384db3139c457feeb9281aa310eef1bc4e0c5d2
Ionut <ionutclaudiu50@yahoo.com>
2023-04-21 16:43:30 +0300

    fixed functions

--- main.py
+++ main.py
@@ -7,37 +7,32 @@ from sys import argv
 parser = argparse.ArgumentParser(description="Sync two directories")
 
 
-origin = 'C:/Users/ionut/Desktop/programare/sync directory/source/'
-target = 'C:/Users/ionut/Desktop/programare/sync directory/replica/'
+origin = 'C:/Users/ionut/OneDrive/Desktop/programare/learning/sync directory/source'
+target = 'C:/Users/ionut/OneDrive/Desktop/programare/learning/sync directory/replica'
 
 
-def sync_files():
+# Sync target to match src
+def start_sync():
     print('Sync in progress...')
-    source_files, replica_files = get_files()
-    for i in get_directories():
+    directories, files = replace_data(origin, target)
+    for i in directories:
         if not os.path.exists(i):
             os.mkdir(i)
-            print('Created directory: '+i)
-    for s, r in zip(source_files, replica_files):
-        if not os.path.exists(r) or os.path.getmtime(s) != os.path.getmtime(r):
-            shutil.copy2(src=s, dst=r)
-            print('Creating file: '+r)
+            print('Creating directory: '+i)
+    for j, k in zip(files, get_files(origin)):
+        if not os.path.exists(j) or os.path.getmtime(j) != os.path.getmtime(k):
+            shutil.copy2(dst=j, src=k)
+            print('Copying file: '+j)
     remove_files()
     print('Sync completed!')
 
-# FILES NOT IN SOURCE SO CANT COMPARE
+
+# Remove files and dirs to match src
 def remove_files():
-    source_files, replica_files = get_files()
-    for s, r in zip(source_files, replica_files):
-        if not os.path.exists(s):
-            if os.path.isdir(r):
-                shutil.rmtree(r)
-                print('Removing directory: '+r)
-            else:
-                os.remove(r)
-                print('Removing file: '+r)
+    ...
 
 
+# Get all files in every subdir inside given path
 def get_files(path):
     files = []
     for p in os.walk(path):
@@ -47,16 +42,20 @@ def get_files(path):
     return files
 
 
+# Get all subdirs based on path
 def get_directories(path):
     directories = []
-    for f in os.walk(origin):
+    for f in os.walk(path):
         directories.append(f[0])
     return directories
 
 
-def replace_data():
-    directories = [x.replace(os.path.basename(os.path.dirname(origin)), os.path.basename(os.path.dirname(target))) for x in get_directories(origin) ]
-    files = [x.replace(os.path.basename(os.path.dirname(origin)), os.path.basename(os.path.dirname(target))) for x in get_files(origin) ]
+# Replace src dir name with target dir name - takes origin and target paths
+def replace_data(path_origin, path_target):
+    if not os.path.exists(path_target):
+        os.mkdir(path_target)
+    directories = [x.replace(os.path.basename(path_origin), os.path.basename(path_target)) for x in get_directories(origin) ]
+    files = [x.replace(os.path.basename(path_origin), os.path.basename(path_target)) for x in get_files(origin) ]
     return directories, files
 
 
